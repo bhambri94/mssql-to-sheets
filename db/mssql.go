0e48595f8222a833d14585c0141c30ae2c061823
Shivam <shivam.kumar@go-jek.com>
2020-08-07 15:59:57 +0530

    Added Retrials plus fixed date issue plus introduced latest data to be added on top of Google Sheets

--- db/mssql.go
+++ db/mssql.go
@@ -4,20 +4,33 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/bhambri94/mssql-to-sheets/configs"
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func GetLatestDataFromSQL(fromDateTime string) [][]interface{} {
-	connectString := "sqlserver://" + configs.Configurations.UserName + ":" + configs.Configurations.Password + "@" + configs.Configurations.MSSQLHost + "?database=" + configs.Configurations.DatabaseName + "&connection+timeout=30"
-
+	var DBConnection *sql.DB
+	var err error
+	connectString := "sqlserver://" + configs.Configurations.UserName + ":" + configs.Configurations.Password + "@" + configs.Configurations.MSSQLHost + "?database=" + configs.Configurations.DatabaseName + "&connection+timeout=300"
 	println("opening sql connection with connstring:" + connectString)
-	DBConnection, err := sql.Open("mssql", connectString)
-	defer DBConnection.Close()
-	if err != nil {
-		println("Open Error:", err)
-		log.Fatal(err)
+
+	RetryCounter := 0
+	ConnectionSuccess := false
+	for RetryCounter < 5 && !ConnectionSuccess {
+		DBConnection, err = sql.Open("mssql", connectString)
+		defer DBConnection.Close()
+		if err != nil {
+			RetryCounter++
+			if strings.Contains(err.Error(), "Client.Timeout") {
+			} else {
+				println("Open Error:", err)
+				log.Fatal(err)
+			}
+		} else {
+			ConnectionSuccess = true
+		}
 	}
 
 	println("Running Query -> " + configs.Configurations.Query + " where " + configs.Configurations.DateColumnName + " > " + fromDateTime + " & scan")
@@ -30,11 +43,15 @@ func GetLatestDataFromSQL(fromDateTime string) [][]interface{} {
 	for Rows.Next() {
 		fmt.Println("adding rows to finalValues")
 		var NumericToString []uint8
+		var QuotationDate string
+		var LostDate string
 		singleRow := make([]interface{}, 12)
-		if err := Rows.Scan(&singleRow[0], &singleRow[1], &singleRow[2], &singleRow[3], &singleRow[4], &singleRow[5], &singleRow[6], &NumericToString, &singleRow[8], &singleRow[9], &singleRow[10], &singleRow[11]); err != nil {
+		if err := Rows.Scan(&singleRow[0], &singleRow[1], &singleRow[2], &QuotationDate, &singleRow[4], &singleRow[5], &singleRow[6], &NumericToString, &singleRow[8], &singleRow[9], &singleRow[10], &LostDate); err != nil {
 			log.Fatal(err)
 		}
 		singleRow[7] = B2S(NumericToString)
+		singleRow[3] = QuotationDate[:10]
+		singleRow[11] = LostDate[:10]
 		finalValues = append(finalValues, singleRow)
 	}
 
--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 	var fromDateTime string
 	if configs.Configurations.OldDataRequired {
 		currentTime := time.Date(2019, time.January, 1, 18, 59, 59, 0, time.UTC) //This can be used to manually fill a sheet from desired date
-		fromDateTime = currentTime.Format("2006-01-02 15:04:05")
+		fromDateTime = currentTime.Format("2006-01-02")
 	} else {
 		// loc, _ := time.LoadLocation("Asia/Kolkata") .In(loc)
 		currentTime := time.Now()
-		HoursCount := 12
-		fromDateTime = currentTime.Add(time.Duration(-HoursCount) * time.Hour).Format("2006-01-02 15:04:05")
+		HoursCount := 24
+		fromDateTime = currentTime.Add(time.Duration(-HoursCount) * time.Hour).Format("2006-01-02")
 	}
 	fmt.Println("Fetching results from Date: " + fromDateTime)
 
